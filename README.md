6a6459f7c0f599df5b8855ed39e03eb36a196832
Thomas Junghans <thomas.junghans@gmail.com>
2015-03-20 08:55:01 +0100

    chore: update readme and integration

--- README.md
+++ README.md
@@ -1,4 +1,51 @@
 # React-List
 
+**Usage**
+
+```javascript
+
+var React = require('react');
+var list = require('react-list');
+
+var container = document.querySelector('body');
+
+var items = [
+  {id: 1, firstname: 'Mike', lastname: 'November'},
+  {id: 2, firstname: 'India', lastname: 'Juliet'},
+  {id: 3, firstname: 'Alpha', lastname: 'Bravo'},
+];
+
+var itemComponent = React.createClass({
+  propTypes: {
+    href: React.PropTypes.string.isRequired,
+    text: React.PropTypes.string.isRequired
+  },
+  render: function () {
+    return (
+      React.DOM.a({ href: this.props.href }, this.props.text)
+    );
+  }
+});
+
+function itemFilter(item) {
+	return {
+		href: '#' + item.id,
+		text: item.firstname + ' ' + item.lastname
+	};
+}
+
+React.render(React.createElement(list, {
+	items: items,
+	itemFilter: itemFilter,
+	itemComponent: itemComponent
+}), container);
+
+```
+
+## Properties
+
+- `items`: an array of items, where items can be an arbitrary object
+- `itemFilter`: a function that is called with each item and used to map the item to the properties of itemComponent
+- `itemComponent`: a react class that is used to display an item
 
 
--- integration/browser.js
+++ integration/browser.js
@@ -1,54 +1,38 @@
 'use strict';
 
 var React = require('react');
-var List = require('../');
+var list = require('../');
 
 var widget = document.querySelector('.widget');
 
-function render(component, config, container) {
-  var div = document.createElement('div');
-  container.appendChild(div);
-  React.render(React.createElement(component, config), div);
-}
-/*
-render(List, {
-  items: ['apples', 'bananas', 'cherries', 'dates']
-}, widget);*/
+var items = [
+  {id: 1, firstname: 'Mike', lastname: 'November'},
+  {id: 2, firstname: 'India', lastname: 'Juliet'},
+  {id: 3, firstname: 'Alpha', lastname: 'Bravo'},
+];
 
-function makeItem(id, name) {
-  return {
-    id: id,
-    name: name,
-    href: '#' + name
-  };
-}
-
-var Item = React.createClass({
+var itemComponent = React.createClass({
   propTypes: {
     href: React.PropTypes.string.isRequired,
     text: React.PropTypes.string.isRequired
   },
   render: function () {
     return (
-      React.DOM.a({
-        href: this.props.href
-      }, this.props.text)
+      React.DOM.a({ href: this.props.href }, this.props.text)
     );
   }
-})
+});
 
-render(List, {
-  items: [
-    makeItem(0, 'Apple'), 
-    makeItem(1, 'Microsoft'), 
-    makeItem(2, 'Oracle')
-  ],
-  itemComponent: Item,
-  itemFilter: function (data) {
-    return {
-      href: data.href,
-      text: data.name
-    };
-  }
-}, widget);
+function itemFilter(item) {
+  return {
+    href: '#' + item.id,
+    text: item.firstname + ' ' + item.lastname
+  };
+}
+
+React.render(React.createElement(list, {
+  items: items,
+  itemFilter: itemFilter,
+  itemComponent: itemComponent
+}), widget);
 
